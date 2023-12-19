fd971bdef5284eca2a1a977886dee017863bdf70
ditsuke <ditsuke@protonmail.com>
2023-12-20 03:37:14 +0530

    feat: Add trait for anomaly detectors

--- src/anomaly/mod.rs
+++ src/anomaly/mod.rs
@@ -1,1 +1,11 @@
+use num::{Float, FromPrimitive};
+use std::ops::{AddAssign, DivAssign, MulAssign, SubAssign};
+
+use crate::common::{ClassifierOutput, Observation};
+
 pub mod half_space_tree;
+
+trait AnomalyDetector<F: Float + FromPrimitive + AddAssign + SubAssign + MulAssign + DivAssign> {
+    fn learn_one(&mut self, observation: &Observation<F>);
+    fn score_one(&mut self, observation: &Observation<F>) -> Option<ClassifierOutput<F>>;
+}
